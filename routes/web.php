ef896d5e35465c8e88bb6e867fefd505f7d68378
NONstop <non_2002@mail.ru>
2021-05-09 03:48:26 +0300

    fix web.php to add routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +17,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'index'])->name('index');
 
-Route::get('/news', function () {
-    return 'News';
-});
+Route::get('/login', [UserController::class, 'index'])->name('user.login');
 
-Route::get('/category', function () {
-    return 'Category';
+Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+
+Route::prefix('news')->group(function () {
+    Route::get('/', [NewsController::class, 'index'])->name('news.index');
+    Route::get('/{id}', [NewsController::class, 'show'])->name('news.show')->where('id', '[0-9]+');
+    Route::get('/add', [NewsController::class, 'add'])->name('news.add');
 });
