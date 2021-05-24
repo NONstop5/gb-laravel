6c30069136be71b5b1964a2f7c1bac35451d741a
NONstop <non_2002@mail.ru>
2021-05-24 09:34:08 +0300

    fix NewsController.php to add data for template

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -6,14 +6,61 @@ use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    public function index()
+    public function index($categoryId)
     {
-        return view('news.index');
+        $categoryList = [
+            'Hot news',
+            'Space news',
+            'Moscow news',
+            'Travel news',
+            'Car news',
+        ];
+
+        $newsList = [
+            [
+                'Hot news1',
+                'Hot news2',
+                'Hot news3',
+                'Hot news4',
+            ],
+            [
+                'Space news1',
+                'Space news2',
+                'Space news3',
+                'Space news4',
+            ],
+            [
+                'Moscow news1',
+                'Moscow news2',
+                'Moscow news3',
+                'Moscow news4',
+            ],
+            [
+                'Travel news1',
+                'Travel news2',
+                'Travel news3',
+                'Travel news4',
+            ],
+            [
+                'Car news1',
+                'Car news2',
+                'Car news3',
+                'Car news4',
+            ],
+        ];
+
+        return view('news.index', [
+            'categoryId' => $categoryId,
+            'categoryList' => $categoryList,
+            'newsList' => $newsList,
+        ]);
     }
 
-    public function show($id)
-    {
-        dd($id);
+    public function show(
+        int $categoryId,
+        int $id
+    ) {
+
         return view('news.show');
     }
 
